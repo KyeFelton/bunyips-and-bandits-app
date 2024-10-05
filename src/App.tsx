00464dddab7715de33639810658c23da376b8c4c
Kye Felton <kye.felton@hotmail.com>
2024-10-05 18:32:34 +1000

    add app header and toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {
+  faDownload,
+  faUpload,
+  faRotateRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button } from "./components/ui/button";
+import logo from "./logo.png";
 
 function App() {
   return (
-    <div className="p-2">
-      <header className="bg-slate-50">
-      <FontAwesomeIcon icon={faEnvelope} />
+    <div className="min-h-screen w-full bg-muted">
+      <header className="fixed top-8 right-8 w-[320px] items-center gap-4 border-b bg-background rounded-lg p-4">
+        <div className="flex flex-row items-center gap-2">
+          <img src={logo} alt="logo" width="64" height="auto" />
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Bunyips & Bandits
+          </h1>
+        </div>
+        <div className="flex flex-row items-center justify-center gap-2">
+          <Button variant="ghost">
+            <FontAwesomeIcon icon={faDownload} size="lg" title="Download" />
+          </Button>
+          <Button variant="ghost">
+            <FontAwesomeIcon icon={faUpload} size="lg" title="Upload" />
+          </Button>
+          <Button variant="ghost">
+            <FontAwesomeIcon icon={faRotateRight} size="lg" title="Reset" />
+          </Button>
+        </div>
       </header>
-      <main className="grid grid-cols-4 gap-2">
-      </main>
+      <main></main>
     </div>
   );
 }
