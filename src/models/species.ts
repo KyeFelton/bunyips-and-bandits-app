a88d9e401c1b038b83e7d601cf149ae77906218c
Kye Felton <kye.felton@hotmail.com>
2025-03-02 20:31:37 +1100

    add more species

--- src/models/species.ts
+++ src/models/species.ts
@@ -57,8 +57,167 @@ export type Species = {
   };
 };
 
-export const Minotaur: Species = {
-  name: "Minotaur",
+export const Badgerman: Species = {
+  name: "Badgerman",
+  size: CreatureSize.Small,
+  health: {
+    initial: 10,
+    increments: 1,
+  },
+  sanity: {
+    initial: 10,
+    increments: 1,
+  },
+  stamina: {
+    initial: 10,
+    increments: 1,
+  },
+  speed: {
+    [MovementType.Walk]: 3,
+    [MovementType.Swim]: 1,
+    [MovementType.Climb]: 3,
+    [MovementType.Fly]: 0,
+  },
+  senses: {
+    [SenseType.Sight]: true,
+    [SenseType.InfraredSight]: false,
+    [SenseType.Hearing]: true,
+    [SenseType.TremorHearing]: false,
+    [SenseType.Smell]: true,
+    [SenseType.Psychic]: false,
+  },
+  armour: {
+    [DamageType.Fire]: 0,
+    [DamageType.Electric]: 0,
+    [DamageType.Toxic]: 0,
+    [DamageType.Slash]: 0,
+    [DamageType.Force]: 0,
+  },
+  skillLevels: {
+    [SkillType.Strength]: 2,
+    [SkillType.Agility]: 2,
+    [SkillType.Dexterity]: 3,
+    [SkillType.Throw]: 2,
+    [SkillType.Intelligence]: 2,
+    [SkillType.Nature]: 2,
+    [SkillType.Willpower]: 2,
+    [SkillType.Charisma]: 2,
+    [SkillType.Psychology]: 2,
+    [SkillType.Stealth]: 3,
+    [SkillType.Sight]: 1,
+    [SkillType.Hearing]: 3,
+    [SkillType.Smell]: 3,
+  },
+};
+
+export const Cobber: Species = {
+  name: "Cobber",
+  size: CreatureSize.Small,
+  health: {
+    initial: 10,
+    increments: 1,
+  },
+  sanity: {
+    initial: 10,
+    increments: 1,
+  },
+  stamina: {
+    initial: 10,
+    increments: 1,
+  },
+  speed: {
+    [MovementType.Walk]: 3,
+    [MovementType.Swim]: 3,
+    [MovementType.Climb]: 3,
+    [MovementType.Fly]: 0,
+  },
+  senses: {
+    [SenseType.Sight]: true,
+    [SenseType.InfraredSight]: false,
+    [SenseType.Hearing]: true,
+    [SenseType.TremorHearing]: false,
+    [SenseType.Smell]: true,
+    [SenseType.Psychic]: false,
+  },
+  armour: {
+    [DamageType.Fire]: 0,
+    [DamageType.Electric]: 0,
+    [DamageType.Toxic]: 0,
+    [DamageType.Slash]: 0,
+    [DamageType.Force]: 0,
+  },
+  skillLevels: {
+    [SkillType.Strength]: 1,
+    [SkillType.Agility]: 3,
+    [SkillType.Dexterity]: 2,
+    [SkillType.Throw]: 2,
+    [SkillType.Intelligence]: 2,
+    [SkillType.Nature]: 2,
+    [SkillType.Willpower]: 2,
+    [SkillType.Charisma]: 3,
+    [SkillType.Psychology]: 2,
+    [SkillType.Stealth]: 3,
+    [SkillType.Sight]: 2,
+    [SkillType.Hearing]: 3,
+    [SkillType.Smell]: 1,
+  },
+};
+
+export const Cormiard: Species = {
+  name: "Cormiard",
+  size: CreatureSize.Medium,
+  health: {
+    initial: 20,
+    increments: 2,
+  },
+  sanity: {
+    initial: 10,
+    increments: 1,
+  },
+  stamina: {
+    initial: 10,
+    increments: 1,
+  },
+  speed: {
+    [MovementType.Walk]: 4,
+    [MovementType.Swim]: 4,
+    [MovementType.Climb]: 1,
+    [MovementType.Fly]: 0,
+  },
+  senses: {
+    [SenseType.Sight]: true,
+    [SenseType.InfraredSight]: false,
+    [SenseType.Hearing]: true,
+    [SenseType.TremorHearing]: false,
+    [SenseType.Smell]: true,
+    [SenseType.Psychic]: false,
+  },
+  armour: {
+    [DamageType.Fire]: 0,
+    [DamageType.Electric]: 0,
+    [DamageType.Toxic]: 0,
+    [DamageType.Slash]: 0,
+    [DamageType.Force]: 0,
+  },
+  skillLevels: {
+    [SkillType.Strength]: 2,
+    [SkillType.Agility]: 2,
+    [SkillType.Dexterity]: 2,
+    [SkillType.Throw]: 3,
+    [SkillType.Intelligence]: 2,
+    [SkillType.Nature]: 2,
+    [SkillType.Willpower]: 2,
+    [SkillType.Charisma]: 2,
+    [SkillType.Psychology]: 2,
+    [SkillType.Stealth]: 2,
+    [SkillType.Sight]: 3,
+    [SkillType.Hearing]: 2,
+    [SkillType.Smell]: 1,
+  },
+};
+
+export const Goa: Species = {
+  name: "Goa",
   size: CreatureSize.Large,
   health: {
     initial: 30,
@@ -73,9 +232,9 @@ export const Minotaur: Species = {
     increments: 1,
   },
   speed: {
-    [MovementType.Walk]: 5,
+    [MovementType.Walk]: 4,
     [MovementType.Swim]: 1,
-    [MovementType.Climb]: 0,
+    [MovementType.Climb]: 1,
     [MovementType.Fly]: 0,
   },
   senses: {
@@ -97,16 +256,16 @@ export const Minotaur: Species = {
     [SkillType.Strength]: 3,
     [SkillType.Agility]: 1,
     [SkillType.Dexterity]: 2,
-    [SkillType.Throw]: 2,
+    [SkillType.Throw]: 3,
     [SkillType.Intelligence]: 2,
     [SkillType.Nature]: 2,
-    [SkillType.Willpower]: 3,
+    [SkillType.Willpower]: 2,
     [SkillType.Charisma]: 2,
     [SkillType.Psychology]: 2,
     [SkillType.Stealth]: 1,
-    [SkillType.Sight]: 1,
+    [SkillType.Sight]: 2,
     [SkillType.Hearing]: 3,
-    [SkillType.Smell]: 2,
+    [SkillType.Smell]: 1,
   },
 };
 
@@ -159,6 +318,59 @@ export const Jeli: Species = {
     [SkillType.Stealth]: 2,
     [SkillType.Sight]: 1,
     [SkillType.Hearing]: 1,
+    [SkillType.Smell]: 2,
+  },
+};
+
+export const Minotaur: Species = {
+  name: "Minotaur",
+  size: CreatureSize.Large,
+  health: {
+    initial: 30,
+    increments: 3,
+  },
+  sanity: {
+    initial: 10,
+    increments: 1,
+  },
+  stamina: {
+    initial: 10,
+    increments: 1,
+  },
+  speed: {
+    [MovementType.Walk]: 5,
+    [MovementType.Swim]: 1,
+    [MovementType.Climb]: 0,
+    [MovementType.Fly]: 0,
+  },
+  senses: {
+    [SenseType.Sight]: true,
+    [SenseType.InfraredSight]: false,
+    [SenseType.Hearing]: true,
+    [SenseType.TremorHearing]: false,
+    [SenseType.Smell]: true,
+    [SenseType.Psychic]: false,
+  },
+  armour: {
+    [DamageType.Fire]: 0,
+    [DamageType.Electric]: 0,
+    [DamageType.Toxic]: 0,
+    [DamageType.Slash]: 0,
+    [DamageType.Force]: 0,
+  },
+  skillLevels: {
+    [SkillType.Strength]: 3,
+    [SkillType.Agility]: 1,
+    [SkillType.Dexterity]: 2,
+    [SkillType.Throw]: 2,
+    [SkillType.Intelligence]: 2,
+    [SkillType.Nature]: 2,
+    [SkillType.Willpower]: 3,
+    [SkillType.Charisma]: 2,
+    [SkillType.Psychology]: 2,
+    [SkillType.Stealth]: 1,
+    [SkillType.Sight]: 1,
+    [SkillType.Hearing]: 3,
     [SkillType.Smell]: 2,
   },
 };
@@ -216,4 +428,174 @@ export const Pixie: Species = {
   },
 };
 
-export const AllSpecies = { Minotaur, Jeli, Pixie };
+export const Tengaroo: Species = {
+  name: "Tengaroo",
+  size: CreatureSize.Small,
+  health: {
+    initial: 10,
+    increments: 1,
+  },
+  sanity: {
+    initial: 10,
+    increments: 1,
+  },
+  stamina: {
+    initial: 10,
+    increments: 1,
+  },
+  speed: {
+    [MovementType.Walk]: 3,
+    [MovementType.Swim]: 1,
+    [MovementType.Climb]: 3,
+    [MovementType.Fly]: 0,
+  },
+  senses: {
+    [SenseType.Sight]: true,
+    [SenseType.InfraredSight]: false,
+    [SenseType.Hearing]: true,
+    [SenseType.TremorHearing]: false,
+    [SenseType.Smell]: true,
+    [SenseType.Psychic]: false,
+  },
+  armour: {
+    [DamageType.Fire]: 0,
+    [DamageType.Electric]: 0,
+    [DamageType.Toxic]: 0,
+    [DamageType.Slash]: 0,
+    [DamageType.Force]: 0,
+  },
+  skillLevels: {
+    [SkillType.Strength]: 1,
+    [SkillType.Agility]: 4,
+    [SkillType.Dexterity]: 2,
+    [SkillType.Throw]: 2,
+    [SkillType.Intelligence]: 2,
+    [SkillType.Nature]: 2,
+    [SkillType.Willpower]: 2,
+    [SkillType.Charisma]: 2,
+    [SkillType.Psychology]: 2,
+    [SkillType.Stealth]: 3,
+    [SkillType.Sight]: 3,
+    [SkillType.Hearing]: 1,
+    [SkillType.Smell]: 2,
+  },
+};
+
+export const Vulturan: Species = {
+  name: "Vulturan",
+  size: CreatureSize.Medium,
+  health: {
+    initial: 20,
+    increments: 2,
+  },
+  sanity: {
+    initial: 10,
+    increments: 1,
+  },
+  stamina: {
+    initial: 10,
+    increments: 1,
+  },
+  speed: {
+    [MovementType.Walk]: 4,
+    [MovementType.Swim]: 1,
+    [MovementType.Climb]: 1,
+    [MovementType.Fly]: 0,
+  },
+  senses: {
+    [SenseType.Sight]: true,
+    [SenseType.InfraredSight]: false,
+    [SenseType.Hearing]: true,
+    [SenseType.TremorHearing]: false,
+    [SenseType.Smell]: true,
+    [SenseType.Psychic]: false,
+  },
+  armour: {
+    [DamageType.Fire]: 0,
+    [DamageType.Electric]: 0,
+    [DamageType.Toxic]: 0,
+    [DamageType.Slash]: 0,
+    [DamageType.Force]: 0,
+  },
+  skillLevels: {
+    [SkillType.Strength]: 2,
+    [SkillType.Agility]: 2,
+    [SkillType.Dexterity]: 2,
+    [SkillType.Throw]: 2,
+    [SkillType.Intelligence]: 2,
+    [SkillType.Nature]: 2,
+    [SkillType.Willpower]: 3,
+    [SkillType.Charisma]: 2,
+    [SkillType.Psychology]: 2,
+    [SkillType.Stealth]: 2,
+    [SkillType.Sight]: 3,
+    [SkillType.Hearing]: 2,
+    [SkillType.Smell]: 1,
+  },
+};
+
+export const Yowie: Species = {
+  name: "Yowie",
+  size: CreatureSize.Large,
+  health: {
+    initial: 30,
+    increments: 3,
+  },
+  sanity: {
+    initial: 10,
+    increments: 1,
+  },
+  stamina: {
+    initial: 10,
+    increments: 1,
+  },
+  speed: {
+    [MovementType.Walk]: 4,
+    [MovementType.Swim]: 1,
+    [MovementType.Climb]: 1,
+    [MovementType.Fly]: 0,
+  },
+  senses: {
+    [SenseType.Sight]: true,
+    [SenseType.InfraredSight]: false,
+    [SenseType.Hearing]: true,
+    [SenseType.TremorHearing]: false,
+    [SenseType.Smell]: true,
+    [SenseType.Psychic]: false,
+  },
+  armour: {
+    [DamageType.Fire]: 0,
+    [DamageType.Electric]: 0,
+    [DamageType.Toxic]: 0,
+    [DamageType.Slash]: 0,
+    [DamageType.Force]: 0,
+  },
+  skillLevels: {
+    [SkillType.Strength]: 3,
+    [SkillType.Agility]: 1,
+    [SkillType.Dexterity]: 2,
+    [SkillType.Throw]: 2,
+    [SkillType.Intelligence]: 2,
+    [SkillType.Nature]: 3,
+    [SkillType.Willpower]: 2,
+    [SkillType.Charisma]: 2,
+    [SkillType.Psychology]: 2,
+    [SkillType.Stealth]: 1,
+    [SkillType.Sight]: 1,
+    [SkillType.Hearing]: 3,
+    [SkillType.Smell]: 2,
+  },
+};
+
+export const AllSpecies = {
+  Badgerman,
+  Cobber,
+  Cormiard,
+  Goa,
+  Jeli,
+  Minotaur,
+  Pixie,
+  Tengaroo,
+  Vulturan,
+  Yowie,
+};
--- src/utils/speciesImages.ts
+++ src/utils/speciesImages.ts
@@ -1,12 +1,26 @@
-import minotaurImage from "../images/species/minotaur.png";
+import badgermanImage from "../images/species/badgerman.png";
+import cobberImage from "../images/species/cobber.png";
+import cormiardImage from "../images/species/cormiard.png";
+import goaImage from "../images/species/goa.png";
 import jeliImage from "../images/species/jeli.png";
+import minotaurImage from "../images/species/minotaur.png";
 import pixieImage from "../images/species/pixie.png";
+import tengarooImage from "../images/species/tengaroo.png";
+import vulturanImage from "../images/species/vulturan.png";
+import yowieImage from "../images/species/yowie.png";
 import { AllSpecies } from "../models/species";
 
 const speciesImages: Record<string, string> = {
-  Minotaur: minotaurImage,
+  Badgerman: badgermanImage,
+  Cobber: cobberImage,
+  Cormiard: cormiardImage,
+  Goa: goaImage,
   Jeli: jeliImage,
+  Minotaur: minotaurImage,
   Pixie: pixieImage,
+  Tengaroo: tengarooImage,
+  Vulturan: vulturanImage,
+  Yowie: yowieImage,
 };
 
 export const getSpeciesImage = (speciesName: string): string => {
