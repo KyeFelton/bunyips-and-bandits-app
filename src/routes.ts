5ab655d6c7720a24c0b7728f9ad19530f5688e6f
Kye Felton <kye.felton@hotmail.com>
2025-11-19 22:27:27 +1100

    fix github pages 404 on direct url access

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,14 @@
-export const HomeRoute = "/bunyips-and-bandits-app";
+export const HomeRoute = "/";
 export const CharacterListRoute = HomeRoute;
-export const CharacterSheetRoute = `${HomeRoute}/character/:id`;
-export const getCharacterSheetRoute = (id: string) =>
-  `${HomeRoute}/character/${id}`;
-export const CharacterEditorRoute = `${HomeRoute}/character/:id/edit`;
+export const CharacterSheetRoute = `/character/:id`;
+export const getCharacterSheetRoute = (id: string) => `/character/${id}`;
+export const CharacterEditorRoute = `/character/:id/edit`;
 export const getCharacterEditorRoute = (id: string) =>
-  `${HomeRoute}/character/${id}/edit`;
-export const HandbookRoute = `${HomeRoute}/handbook`;
-export const WikiRoute = `${HomeRoute}/wiki`;
-export const WikiCategoryRoute = `${HomeRoute}/wiki/:category`;
-export const getWikiCategoryRoute = (category: string) =>
-  `${HomeRoute}/wiki/${category}`;
-export const WikiArticleRoute = `${HomeRoute}/wiki/:category/:id`;
+  `/character/${id}/edit`;
+export const HandbookRoute = `/handbook`;
+export const WikiRoute = `/wiki`;
+export const WikiCategoryRoute = `/wiki/:category`;
+export const getWikiCategoryRoute = (category: string) => `/wiki/${category}`;
+export const WikiArticleRoute = `/wiki/:category/:id`;
 export const getWikiArticleRoute = (category: string, id: string) =>
-  `${HomeRoute}/wiki/${category}/${id}`;
+  `/wiki/${category}/${id}`;
